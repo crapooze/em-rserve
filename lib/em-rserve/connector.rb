3d9d59022a8a85e0458b07b950fba6734e26ab19
crapooze <crapooze@gmail.com>
2011-08-12 09:26:29 +0200

    added request queue in connector may go in another module later

--- lib/em-rserve/connector.rb
+++ lib/em-rserve/connector.rb
@@ -1,5 +1,6 @@
 
 require "em-rserve/parser"
+require "em-rserve/request"
 
 module EM::Rserve
   module Connector 
@@ -15,11 +16,21 @@ module EM::Rserve
 
     extend ClassMethods
 
+    attr_accessor :request_queue
+
     def post_init
       super
       #type of parser carries the state, no need to carry it internally and do
       #zillions of state check
       replace_parser! IDParser
+      @request_queue = []
+    end
+
+    def request
+      r = Request.new
+      yield r if block_given?
+      request_queue << r
+      r
     end
 
     def replace_parser!(klass)
@@ -45,15 +56,31 @@ module EM::Rserve
       end
     end
 
-    # HOOKS TO OVERRIDE
+    # HOOKS TO OVERRIDE, PLEASE CALL SUPER
 
     def ready
     end
 
-    def receive_message_header(qap1)
+    def receive_message_header(head)
+      if head.error?
+        receive_error_message_header(head)
+      elsif head.ok?
+        receive_success_message_header(head)
+      else
+        raise RuntimeError, "nor OK, nor error message #{head}"
+      end
+    end
+
+    def receive_error_message_header(head)
+      request_queue.shift.error(head)
+    end
+
+    def receive_success_message_header(head)
+      request_queue.shift.success(nil) unless head.body?
     end
 
     def receive_message(msg)
+      request_queue.shift.success(msg)
     end
   end
 end
---
+++ lib/em-rserve/request.rb
@@ -0,0 +1,20 @@
+
+module EM::Rserve
+  Request = Struct.new(:callback_blk, :errback_blk) do
+    def callback(&blk)
+      self.callback_blk = blk
+    end
+
+    def errback(&blk)
+      self.errback_blk = blk
+    end
+
+    def error(val)
+      errback_blk.call(val) if errback_blk
+    end
+
+    def success(val)
+      callback_blk.call(val) if callback_blk
+    end
+  end
+end
