92e0dea0e8e76baaeeb7d6fa8471544389f8eb3c
crapooze <crapooze@gmail.com>
2011-11-18 22:42:13 +0100

    added unstable backend for easy building of clusters

--- lib/em-rserve/backend.rb
+++ lib/em-rserve/backend.rb
@@ -9,6 +9,10 @@ module EM::Rserve
     # A Server just holds an host and a port.
     # It respond to :server answering self. The reason for this is to have
     Server = Struct.new(:host, :port) do
+      def ==(other)
+        (other.host == self.host) and (other.port == self.port) 
+      end
+
       # Returns self. 
       #
       # The reason for this method is to unify an interface between Server and
@@ -72,6 +76,77 @@ module EM::Rserve
     # Returns the next server and shift the round-robin by one.
     def next
       @servers.unshift(@servers.pop).first.server
+    end
+  end
+
+  # The UnstableBackend is a backend suitable when the pool of servers may
+  # change with time (e.g., to adapt with load)
+  #
+  # The operation are simple: 
+  # * UnstableBackend#server_found adds a server to # the list of available servers for next connections 
+  # * UnstableBackend#server_lost removes a server for next connections
+  #
+  # The backends are polled in a round-robin fashion
+  #
+  # If no server is available, fibers are blocked until one is available. When
+  # the first server becomes available, all pending fibers will continue to
+  # this server. Note that this may have adverse consequences if many
+  # connection starts in parallel.
+  class UnstableBackend < Backend 
+
+    def initialize 
+      super 
+      @servers = []
+      @stalled_fibers = [] 
+    end
+
+    # Adds srv to the list of available servers for next connections
+    # wakes up all pending fibers if any
+    def server_found(srv)
+      @servers << srv unless @servers.include?(srv)
+      wake_up!(srv) if stalled?
+    end
+
+    # Removes srv to the list of available
+    def server_lost(srv)
+      @servers.delete(srv) if @servers.include?(srv)
+    end
+
+    # Returns next server, if none is available, will blocks current Fiber
+    # until a server is available.
+    def next
+      srv = shift_servers!
+      unless srv
+        fib = Fiber.current
+        @stalled_fibers << fib
+        srv = Fiber.yield
+      end
+      srv
+    end
+
+    private
+
+    # shift currently available servers in a round-robin fashion
+    def shift_servers!
+      srv = @servers.pop
+      if srv
+        @servers.unshift(srv)
+        srv
+      end
+    end
+
+    # true if there is any fiber to wake up
+    def stalled?
+      @stalled_fibers.any?
+    end
+
+    # wakes up all blocked fibers
+    def wake_up!(srv)
+      fibs = @stalled_fibers.dup
+      fibs.each do |fib|
+        fib.resume(srv) if fib
+      end
+      @stalled_fibers = []
     end
   end
 end
